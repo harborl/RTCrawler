8b39dab23b7aa67b719345d4f80d6feb1defe1d2
雒海波 <haibo.luo@renren-inc.com>
2016-05-11 11:13:17 +0800

    Fix: the package name of main entry in crawler.sh.

--- src/main/resources/scripts/coldwell/crawler.sh
+++ src/main/resources/scripts/coldwell/crawler.sh
@@ -6,4 +6,4 @@ CLASSPATH=.:lib/*
 java -server \
      -Xms512M -Xmx1024M -Xss256k -XX:PermSize=256m -XX:MaxPermSize=256m \
      -cp $CLASSPATH \
-     com.homethy.crawler.coldwell.clts.Crawler $@
+     io.harborl.crawler.coldwell.clts.Crawler $@
